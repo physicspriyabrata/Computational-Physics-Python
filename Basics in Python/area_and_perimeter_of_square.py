93d45cfff15613ae962fd2f3782a7e830280a808
Priyabrata Jean <physicspriyabrat@gmail.com>
2026-04-09 01:05:55 +0530

    Add files via upload

---
+++ Basics in Python/area_and_perimeter_of_square.py
@@ -0,0 +1,5 @@
+#find the area and perimeter of square 
+
+a=float(input("Enter the side of square= "))
+print("area of square is: ", a*a)
+print("perimeter of square is : ", 4*a)
---
+++ Basics in Python/general_multiplayer.py
@@ -0,0 +1,15 @@
+#print general multiplier input by user
+a=float(input("Enter a number that you want to multiplier__"))
+i=1
+while(i<=10):
+	print(i*a)
+	i=i+1
+	
+#for list
+i=1
+y=[]
+while(i<=10):
+	n=i*a
+	y.append(n)
+	i=i+1
+print(y)

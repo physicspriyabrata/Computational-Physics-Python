9e447601e5894b875da9608f0a0a29fd6aebdd3d
Priyabrata Jean <physicspriyabrat@gmail.com>
2026-04-09 01:05:10 +0530

    Add files via upload

---
+++ Basics in Python/while_loop.py
@@ -0,0 +1,52 @@
+#While loop in python
+
+#initialize 
+#while condition: 
+   #some work(also use conditional)
+   #undation
+   
+   
+   
+#print hello word 10 times
+i=1
+while(i<=10):
+	print(i," hello")
+	i+=1  #also use i=i+1
+	
+#print numbers from 1 to 100
+i=1
+while(i<=100):
+	print(i)
+	i=i+1
+	
+#print numbers from 100 to 1, i.e reverse
+i=100
+while(i>=1):
+	print(i)
+	i=i-1
+	
+#print the multiplication table of a number 8
+i=1
+while(i<=10):
+	print("8 multiplication",i*8)
+	i=i+1
+
+#use list to print 7 multipllication in list 
+i=1
+y=[]
+while(i<=10):
+	a=i*7
+	print(a)
+	y.append(a)  #add list
+	i=i+1
+print(y)
+
+
+
+
+
+
+
+
+	
+

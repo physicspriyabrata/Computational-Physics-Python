b868ac925fbd78bfe390f7f78d350eef859dd160
Priyabrata Jean <physicspriyabrat@gmail.com>
2026-04-09 19:58:24 +0530

    Add files via upload

---
+++ Basics in Python/for_loop.py
@@ -0,0 +1,24 @@
+#for loop
+
+#for element in list:
+	#some work
+
+
+list=[1,3,4,5]
+for i in list:
+	print(i)
+	
+#for loop with range 
+
+#for i in range(start, stop, step):
+	#some work
+	
+#print 1 to 100 numbers
+for i in range(1,100,1):
+	print(i)
+	
+#print the multipication tableof a number input by user 
+a=int(input("enter the number(must integer) for multipi=licatin__")) #because in range float not allowed
+for i in range(a, 11*a, a):
+	print(i	) 
+

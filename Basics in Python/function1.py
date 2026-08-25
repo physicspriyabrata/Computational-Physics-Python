47aa423a02d583650de8e2b3f15ea3ce6f397b97
Priyabrata Jean <physicspriyabrat@gmail.com>
2026-08-25 13:16:31 +0530

    Add files via upload

---
+++ Basics in Python/function1.py
@@ -0,0 +1,5 @@
+#this is the first programme in function
+def greet(name):
+	print(name, "hello") #that is function
+greet("priyabrat")   #call to the function
+greet("priya")		
---
+++ Basics in Python/function_def.py
@@ -0,0 +1,14 @@
+#defination in pyton
+
+#def function_name(parameter1,parameter2,.....):
+	#some work
+	#return value
+#function_name(arg1, arg2, arg3 , ........) # function call
+
+
+def addition(a,b):
+	sum=a+b
+	return sum
+
+result=addition(10,23)
+print(result)
